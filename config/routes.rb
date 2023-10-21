b32bb89195bbb6b890df3fb5bbb6b4b77b6d792d
Pedro Augusto <pedroaugusto@hey.com>
2023-10-21 13:47:14 -0300

    added user model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: "", path_names: {
+    sign_in: "login",
+    sign_out: "logout",
+    sign_up: "register",
+    password: "secret",
+    confirmation: "verification",
+    unlock: "unblock"
+  }
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up", to: "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 end
